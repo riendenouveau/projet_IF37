d6ce2dfb3735cbf88f986278c5d2ce85bf0c9c18
riendenouveau <96743378+riendenouveau@users.noreply.github.com>
2022-01-07 16:29:21 +0100

    Add files via upload

---
+++ styles/etablissementInfo.php
@@ -0,0 +1,25 @@
+<style>
+    #infoEtablissement{
+    width : 40%;
+}
+#carteEtablissement{
+    width : 60%;
+}
+
+@media screen and (min-width:900px) {
+    #carteEtablissement{
+        width: 60%;
+    }
+    #infoEtablissement{
+        width: 40%;
+    }
+}
+@media screen and (max-width:899px) {
+    #carteEtablissement{
+        width: 100%;
+    }
+    #infoEtablissement{
+        width: 100%;
+    }
+}
+</style>
---
+++ styles/etablissementSansInfo.php
@@ -0,0 +1,29 @@
+<style>
+    #infoEtablissement{
+    width : 0%;
+    display: none;
+}
+
+#carteEtablissement{
+    width : 100%;
+}
+
+@media screen and (min-width:900px) {
+    #carteEtablissement{
+        width: 100%;
+    }
+    #infoEtablissement{
+        width: 0%;
+        display: none;
+    }
+}
+@media screen and (max-width:899px) {
+    #carteEtablissement{
+        width: 100%;
+    }
+    #infoEtablissement{
+        width: 0%;
+        display: none;
+    }
+}
+</style>
